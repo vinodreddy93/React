5bfc3db3a2c8b5a062be7d934dbff316b4f2dcca
Vinod <vinod>
2020-01-06 17:21:26 +0530

    Info about react

---
+++ steps.sh
@@ -0,0 +1,43 @@
+React Js Fundamentals:
+======================================================
+
+1)JSX — Allows us to write HTML like syntax which gets
+transformed to lightweightJavaScript objects.
+
+2)Virtual DOM — A JavaScript representation of the actual
+DOM.
+
+3)React.Component — The way in which you create a new component.
+
+4)render (method) — Describes what the UI will look like for
+the particular component.
+
+5)ReactDOM.render — Renders a React component to a DOM node.
+
+6)state — The internal data store (object) of a component.
+
+7)constructor (this.state) - The way in which you establish
+the initial state of a component.
+
+8)setState — A helper method used for updating the state of a
+component and re-rendering the UI
+
+9)props — The data which is passed to the child component
+from the parent component.
+
+10)propTypes — Allows you to control the presence, or types of
+certain props passed to the child component.
+
+11)defaultProps — Allows you to set default props for your component.
+
+12)Component LifeCycle
+  - componentDidMount — Fired after the component mounted
+  - componentWillUnmount — Fired before the component will unmount
+  - getDerivedStateFromProps - Fired when the component mounts and
+whenever the props change. Used to update the state of a
+component when its props change
+
+13)Events
+  - onClick
+  - onSubmit
+  - onChange
