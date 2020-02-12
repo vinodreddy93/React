7e81838dab6105bf8804024bc266d6b8f5aabc41
vinod <vinod/reddy>
2020-02-12 09:56:02 +0530

    budgetCalculator

---
+++ bugetCalculator/src/App.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import Alert from "./components/Alert";
+import uuid from "uuid/v4";
+
+const initialExpenses = localStorage.getItem("expenses")
+  ? JSON.parse(localStorage.getItem("expenses"))
+  : [];
+
+function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  const [charge, setCharge] = useState("");
+  const [amount, setAmount] = useState("");
+  // alert
+  const [alert, setAlert] = useState({ show: false });
+  // edit
+  const [edit, setEdit] = useState(false);
+  const [id, setId] = useState(0);
+  useEffect(() => {
+    console.log("called");
+
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }, [expenses]);
+
+  //addcharge
+  const handleCharge = e => {
+    setCharge(e.target.value);
+  };
+  // add amount
+  const handleAmount = e => {
+    let amount = e.target.value;
+    if (amount === "") {
+      setAmount(amount);
+    } else {
+      setAmount(parseInt(amount));
+    }
+  };
+
+  // handle alert
+  const handleAlert = ({ type, text }) => {
+    setAlert({ show: true, type, text });
+    setTimeout(() => {
+      setAlert({ show: false });
+    }, 7000);
+  };
+  // handle submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (charge !== "" && amount > 0) {
+      if (edit) {
+        let tempExpenses = expenses.map(item => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+      } else {
+        const singleExpense = { id: uuid(), charge, amount };
+        setExpenses([...expenses, singleExpense]);
+        handleAlert({ type: "success", text: "item added" });
+      }
+      // set charge back to empty string
+      setCharge("");
+      // set amount back to zero
+      setAmount("");
+    } else {
+      handleAlert({
+        type: "danger",
+        text: `charge can't be empty value and amount value has to be bigger than zero`
+      });
+    }
+  };
+  // handle delete
+  const handleDelete = id => {
+    let tempExpenses = expenses.filter(item => item.id !== id);
+    setExpenses(tempExpenses);
+    handleAlert({ type: "danger", text: "item deleted" });
+  };
+  //clear all items
+  const clearItems = () => {
+    setExpenses([]);
+  };
+  // handle edit
+  const handleEdit = id => {
+    let expense = expenses.find(item => item.id === id);
+    let { charge, amount } = expense;
+    setCharge(charge);
+    setAmount(amount);
+    setEdit(true);
+    setId(id);
+  };
+  return (
+    <>
+      {alert.show && <Alert type={alert.type} text={alert.text} />}
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm
+          handleSubmit={handleSubmit}
+          charge={charge}
+          handleCharge={handleCharge}
+          amount={amount}
+          handleAmount={handleAmount}
+          edit={edit}
+        />
+        <ExpenseList
+          expenses={expenses}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          clearItems={clearItems}
+        />
+      </main>
+      <h1>
+        total spending :
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
